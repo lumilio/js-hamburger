b16c7639f786acdfaf3774abb8b2c982e2bbd5d6
lumilio <logistic-division@lumilium.com>
2021-10-13 01:01:40 +0200

    esercizio completato utilizzando .style.display block/none

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
 
-
-const hamburgerButton= document.querySelector(".fa-bars");
 const hamburgerList = document.querySelector(".hamburger-menu");
+const hamburgerButton = document.querySelector(".fa-bars");
+const hamburgerClose = document.querySelector('.fa-times')
+
 hamburgerButton.addEventListener("click", mobileMenu)
+hamburgerClose.addEventListener("click", closeMenu)
 
 
 function mobileMenu() {
-    hamburgerButton.classList.toggle("active");
-    hamburgerList.classList.toggle("active");
+    hamburgerList.style.display = "block";
 }
 
-const navLink = document.querySelectorAll(".hamburger-menu a");
+function closeMenu() {
+    hamburgerList.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+/* function closeMenu() {
+    hamburgerList.hide('active');
+} */
+
+/* /* const navLink = document.querySelectorAll(".hamburger-menu a");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburgerButton.remove("active");
     hamburgerList.remove("active");
-}
+} 
 
 
+ */
+
+/* $(function() {
+    $(".toggle-menu").click(function() {
+      $(this).toggleClass("active");
+      $('.menu-drawer').toggleClass("open");
+    });
+  }); */
+
+
+/*   $('.fa-bars').on('click', function () {
+    $('.menu').addClass('open');
+  });
+  
+  $( '.fa-times' ).on("click", function(){
+    $('.menu').removeClass('open');
+  });
+
+
+
+hamburgerClose.on("click", function(){ hamburgerList.hide(); }); */
