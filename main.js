c8518a0c2c3c810a3f0aedae9af61bfb1a6ace12
lumilio <logistic-division@lumilium.com>
2021-10-13 01:03:03 +0200

    #

--- main.js
+++ main.js
@@ -23,38 +23,3 @@ function closeMenu() {
 
 
 
-/* function closeMenu() {
-    hamburgerList.hide('active');
-} */
-
-/* /* const navLink = document.querySelectorAll(".hamburger-menu a");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburgerButton.remove("active");
-    hamburgerList.remove("active");
-} 
-
-
- */
-
-/* $(function() {
-    $(".toggle-menu").click(function() {
-      $(this).toggleClass("active");
-      $('.menu-drawer').toggleClass("open");
-    });
-  }); */
-
-
-/*   $('.fa-bars').on('click', function () {
-    $('.menu').addClass('open');
-  });
-  
-  $( '.fa-times' ).on("click", function(){
-    $('.menu').removeClass('open');
-  });
-
-
-
-hamburgerClose.on("click", function(){ hamburgerList.hide(); }); */
